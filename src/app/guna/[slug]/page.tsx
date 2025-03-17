02d0d3000fc098be5260a91e78109115f1706ca4
Miha Medven <3627889+aweCodeMan@users.noreply.github.com>
2025-03-17 16:49:16 +0100

    add cache busting

--- src/app/guna/[slug]/page.tsx
+++ src/app/guna/[slug]/page.tsx
@@ -4,6 +4,8 @@ import {PostComments} from "@/components/post-comments";
 import {MarkdownPost} from "@/app/guna/[slug]/components/markdown-post";
 import {LinkPost} from "@/app/guna/[slug]/components/link-post";
 
+export const revalidate = 5;
+
 export default async function Post({params}: {params: Promise<{slug:string}>}) {
     const {slug} = await params;
     const post = (await (await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts/${slug}`)).json()).data;
