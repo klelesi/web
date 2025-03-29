bdec6164c63ebe4406537788f3f4a2a259d57f72
Miha Medven <3627889+aweCodeMan@users.noreply.github.com>
2025-03-29 12:09:45 +0100

    prefetch csrf cookie before any non get requests

--- src/hooks/useClientAxios.tsx
+++ src/hooks/useClientAxios.tsx
@@ -10,10 +10,27 @@ const instance = axios.create({
     withXSRFToken: true,
 });
 
+function refreshCSRFCookie() {
+    return axios.get(`${process.env.NEXT_PUBLIC_API_URL}/sanctum/csrf-cookie`, {
+        withCredentials: true,
+        withXSRFToken: true,
+    });
+}
+
 export default function useClientAxios() {
     const {clear} = useContext(AuthContext);
 
     useEffect(() => {
+        instance.interceptors.request.use(
+            async (config) => {
+                if (config.method !== 'get') {
+                    await refreshCSRFCookie();
+                }
+                return config;
+            },
+            (error) => Promise.reject(error)
+        );
+
         instance.interceptors.response.use(res => res, (error: AxiosError) => {
             if (error.status === 401) {
                 clear();
