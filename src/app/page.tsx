0f23ee123f16f8f90d43570b3be22c80a043f3d2
Miha Medven <3627889+aweCodeMan@users.noreply.github.com>
2025-04-07 20:47:29 +0200

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,46 @@
-import Image from 'next/image'
+import Image from "next/image";
 import Logo from "@/components/logo";
-import {Card} from "@/components/card";
-import {PaginatedApiResult, Post} from "@/interfaces";
-import {NextPage} from "@/components/next-page";
-import React, {ReactNode} from "react";
+import { PaginatedApiResult, Post } from "@/interfaces";
+import { NextPage } from "@/components/next-page";
+import React from "react";
 import AuthEnhancer from "@/components/auth-enhancer";
+import { NotificationCard } from "@/components/notification-card";
 
-export const revalidate = 1;
+export const revalidate = 0;
 
-export default async function Home() {
-    const posts = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/feed`);
-    const data: PaginatedApiResult<Post> = await posts.json();
+const fetchFeed = async (): Promise<PaginatedApiResult<Post>> => {
+  return await (await fetch(`${process.env.NEXT_PUBLIC_API_URL}/feed`)).json();
+};
 
-    return (
-        <div>
-            <main className={'grid grid-cols-1 gap-3'}>
-                <div className="w-full relative">
-                    <Image
-                        className={'h-[200px] object-cover w-full'}
-                        width={1920}
-                        height={200}
-                        src="/images/header_frontpage.png"
-                        alt="Naslovna grafika v pixel artu, kjer nekdo sedi za računalnikom in pije kavo."
-                    />
-                    <div
-                        className={'absolute top-0 left-0 right-0 bottom-0 text-center flex flex-col justify-center items-center'}>
-                        <div className={'flex flex-col justify-center items-center'}>
-                            <Logo/>
-                            <h1 className={'text-black text-lg mt-2 tracking-tight'}>Kjer so dobre debate doma</h1>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid grid-cols-1 gap-3 container max-w-[700px]">
-                    {data.data.length === 0 && (<EmptyState>
-                        <div className="prose"><p>Trenutno ni prispevkov.</p><p>Nerodno.</p></div>
-                    </EmptyState>)}
+export default async function Page() {
+  const paginatedResult = await fetchFeed();
 
-                    <AuthEnhancer posts={data.data}/>
-
-                    {data.meta.nextCursor && (<NextPage cursor={data.meta.nextCursor}/>)}
-                </div>
-            </main>
+  return (
+    <div className={"grid grid-cols-1 gap-3 -mt-3"}>
+      <div className="w-full relative">
+        <Image
+          className={"h-[200px] object-cover w-full"}
+          width={1920}
+          height={200}
+          priority={true}
+          src="/images/header_frontpage.png"
+          alt="Naslovna grafika v pixel artu, kjer nekdo sedi za računalnikom in pije kavo."
+        />
+        <div className={"absolute top-0 left-0 right-0 bottom-0 text-center flex flex-col justify-center items-center"}>
+          <div className={"flex flex-col justify-center items-center"}>
+            <Logo />
+            <h1 className={"text-black text-xl mt-2 tracking-tight"}>Kjer so dobre debate doma</h1>
+          </div>
         </div>
-    );
-}
+      </div>
 
-function EmptyState({children}: { children: ReactNode }) {
-    return <Card>
-        <div className="text-center py-10 text-lg">
-            {children}
-        </div>
-    </Card>
-}
+      <div className="grid grid-cols-1 gap-3 container w-page">
+        {paginatedResult.data.length === 0 && <NotificationCard title={"Trenutno ni prispevkov."} body={"Mogoče je čas, da spišeš prvega."} />}
 
+        <AuthEnhancer posts={paginatedResult.data} />
+
+        {paginatedResult.meta.nextCursor && <NextPage cursor={paginatedResult.meta.nextCursor} />}
+      </div>
+    </div>
+  );
+}
