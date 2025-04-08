9d6d21b6d2d3e94ae90177fbe900317b1e628b85
Miha Medven <3627889+aweCodeMan@users.noreply.github.com>
2025-04-08 22:14:55 +0200

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
+
 export default function Footer() {
     return (
         <>
             <footer className={'bg-white w-full border-t border-black'}>
                 <div className={'py-1 text-center w-full container text-sm'}>
-                    Klele.si | {new Date().getFullYear()}
+                    Klele.si | {new Date().getFullYear()} | <Link className={'hover:text-red'} href={'/pravila'}>Pravila</Link> | <Link className={'hover:text-red'} href={'https://github.com/klelesi'}><FontAwesomeIcon icon={faGithub}/></Link>
                 </div>
             </footer>
         </>
