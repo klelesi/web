a4c231cc4845bc70373135200ebe89c03ddef81c
Miha Medven <3627889+aweCodeMan@users.noreply.github.com>
2025-04-30 17:09:48 +0200

    fix interactions array

--- src/components/auth-enhancer.tsx
+++ src/components/auth-enhancer.tsx
@@ -1,38 +1,50 @@
-'use client';
+"use client";
 
-import {Interaction, Post} from "@/interfaces";
+import { Interaction, Post } from "@/interfaces";
 import PostList from "@/components/post-list";
-import React, {useContext, useEffect, useState} from "react";
-import {AuthContext} from "@/hooks/auth-provider";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "@/hooks/auth-provider";
 import useUserInteractionApi from "@/hooks/useUserInteractionApi";
 
-export default function AuthEnhancer({posts}: { posts: Post[] }) {
-    const {currentUser} = useContext(AuthContext);
-    const {getPostsInteractions} = useUserInteractionApi();
-    const [copy, setCopy] = useState(posts);
+export default function AuthEnhancer({ posts }: { posts: Post[] }) {
+  const { currentUser } = useContext(AuthContext);
+  const { getPostsInteractions } = useUserInteractionApi();
+  const [copy, setCopy] = useState(posts);
 
-    useEffect(() => {
-        if (currentUser) {
-            getPostsInteractions({postIds: posts.map((post) => post.id)}).then((response) => {
-                const map = response.data.data.reduce((accumulator: {[key:string]: Interaction}, currentValue: Interaction) => {
-                    accumulator[currentValue.interactableId!] = currentValue;
-                    return accumulator;
-                }, {})
+  useEffect(() => {
+    if (currentUser) {
+      getPostsInteractions({ postIds: posts.map((post) => post.id) }).then((response) => {
+        const map = response.data.data.reduce(
+          (
+            accumulator: {
+              [key: string]: Interaction[];
+            },
+            currentValue: Interaction,
+          ) => {
+            if (!accumulator.hasOwnProperty(currentValue.interactableId!)) {
+              accumulator[currentValue.interactableId!] = [currentValue];
+            } else {
+              accumulator[currentValue.interactableId!].push(currentValue);
+            }
+            return accumulator;
+          },
+          {},
+        );
 
-                setCopy((prev) => {
-                    //  Set the fetched interactions to the posts
-                    return prev.map((prevPost) => {
-                        prevPost.interactions = [];
-                        if (map[prevPost.id]) {
-                            prevPost.interactions.push(map[prevPost.id]);
-                        }
+        setCopy((prev) => {
+          //  Set the fetched interactions to the posts
+          return prev.map((prevPost) => {
+            prevPost.interactions = [];
+            if (map[prevPost.id]) {
+              prevPost.interactions = map[prevPost.id];
+            }
 
-                        return prevPost;
-                    });
-                });
-            });
-        }
-    }, [currentUser, getPostsInteractions, posts]);
+            return prevPost;
+          });
+        });
+      });
+    }
+  }, [currentUser, getPostsInteractions, posts]);
 
-    return <PostList posts={copy}></PostList>
+  return <PostList posts={copy}></PostList>;
 }
